903b3948ee243fd91857ba8b12e2905985161b64
jahidjak2 <jahidulislamjak2@gmail.com>
2025-05-05 23:43:39 +0600

    updated code

--- index.php
+++ index.php
@@ -102,7 +102,7 @@ if (isset($_GET['edit'])) {
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-<title>User Signup and CRUD</title>
+<title>User Signup Form and CRUD</title>
 <style>
   
   * {
